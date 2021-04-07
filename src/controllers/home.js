117d79c20586ce7e97740e1532b8d9d8b86315e2
Zach Phelps <zphelps01@outlook.com>
2021-04-07 16:08:18 -0500

    Added multiple sources
    Added support for multiple sources, elements are presented at random.

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,4 +1,5 @@
 import { urlOne,urlTwo,urlThree,urlFour,urlFive,urlSix } from '../settings';
+import { shuffle } from '../helpers/shuffle'
 import Parser from 'rss-parser';
 import jsdom from 'jsdom';
 
@@ -11,26 +12,41 @@ export const indexPage = (req, res) => {
         ]
       }
     });
-    parser.parseURL(urlOne,   function(err, feed) {
-      
-      var jsonArray = feed.items;
+    var urlList = [urlOne,urlTwo,urlThree,urlFour,urlFive,urlSix];
+    var combinationList = [];
+    var count = 0;
 
-      for (var item in jsonArray){
-        var str = jsonArray[item].encodedContent;
-        var dom = new jsdom.JSDOM(str);
-        var src = dom.window.document.querySelector("img").getAttribute('src');
-        jsonArray[item].imageUrl = src;
-        delete jsonArray[item]['content:encoded'];
-        delete jsonArray[item]['content:encodedSnippet'];
-        delete jsonArray[item]['dc:creator'];
-        delete jsonArray[item]['encodedContent'];
-        delete jsonArray[item]['comments'];
-        delete jsonArray[item]['content'];
-        delete jsonArray[item]['contentSnippet'];
-        delete jsonArray[item]['guid'];
-        delete jsonArray[item]['categories']
-      }
-      res.json(jsonArray);
-  })
+
+    for (var url in urlList){
+      parser.parseURL(urlList[url],   function(err, feed) {
+        
+        var jsonArray = feed.items;
+
+        for (var item in jsonArray){         
+          var str = jsonArray[item].encodedContent; 
+          var dom = new jsdom.JSDOM(str);
+          if (dom.window.document.querySelector("img") != undefined){
+            var src = dom.window.document.querySelector("img").getAttribute('src');
+            jsonArray[item].imageUrl = src;
+          }
+          delete jsonArray[item]['content:encoded'];
+          delete jsonArray[item]['content:encodedSnippet'];
+          delete jsonArray[item]['dc:creator'];
+          delete jsonArray[item]['encodedContent'];
+          delete jsonArray[item]['comments'];
+          delete jsonArray[item]['content'];
+          delete jsonArray[item]['contentSnippet'];
+          delete jsonArray[item]['guid'];
+          delete jsonArray[item]['categories']
+        }
+        combinationList = combinationList.concat(jsonArray);
+        count ++;
+        if (count === urlList.length){
+          combinationList = shuffle(combinationList);
+          res.json(combinationList);
+        }
+        
+      })
+    }
 }
     
